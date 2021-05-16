c8a37a60e5d6ecb5a945f453fbf6ec99921b6db6
Igorg Gordon <berkut1987@gmail.com>
2021-05-16 17:57:28 +0300

    Add Rank and particles to background style

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import './App.css';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import './App.css'
+
+const particlesOptions = {
+
+    particles: {
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                value_area: 500
+            }
+        }
+    }
+};
 
 function App() {
   return (
     <div className="App">
+        <Particles className='particles'
+                   params={particlesOptions}
+        />
         <Navigation/>
         <Logo/>
+        <Rank/>
         <ImageLinkForm/>
         {/*<FaceRecognition/>*/}
     </div>
